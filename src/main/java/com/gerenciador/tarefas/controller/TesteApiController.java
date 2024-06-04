75b71289094b8c1f27088d61da75bb3f5bbf985e
Daniel <danielfsouza@gmail.com>
2024-06-03 21:49:40 -0300

    Adicionado segunda rota de teste

--- src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
+++ src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
@@ -1,13 +1,19 @@
 package com.gerenciador.tarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TesteApiController {
 
     @GetMapping("/teste-api")
-    private String Teste (){
+    public String Teste (){
         return "Sucesso";
+    }
+
+    @GetMapping("teste-api-benvindo")
+    public String TesteBenvindo (@RequestParam(name = "nome") String nome){
+       return "Olá "+nome+", seja muito Bem Vindo!";
     }
 }
